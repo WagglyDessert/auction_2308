ed59810fe0c3f88758be7958719c5f2969ef88ba
Nathan Trautenberg <Natester123@mac.com>
2023-09-26 09:26:45 -0600

    updated spec files' before each to include more items and attendees for iteration 2.

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -5,16 +5,27 @@ RSpec.describe Item do
   before(:each) do
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
-    @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
   end
 
   it 'can initialize' do
-    expect(@attendee).to be_an_instance_of(Attendee)
+    expect(@attendee1).to be_an_instance_of(Attendee)
+    expect(@attendee2).to be_an_instance_of(Attendee)
+    expect(@attendee3).to be_an_instance_of(Attendee)
   end
 
   it 'has attributes' do
-    expect(@attendee.name).to eq("Megan")
-    expect(@attendee.budget).to eq(50)
+    expect(@attendee1.name).to eq("Megan")
+    expect(@attendee2.name).to eq("Bob")
+    expect(@attendee3.name).to eq("Mike")
+    expect(@attendee1.budget).to eq(50)
+    expect(@attendee2.budget).to eq(75)
+    expect(@attendee3.budget).to eq(100)
   end
 
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,16 +4,25 @@ RSpec.describe Item do
   before(:each) do
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
   end
 
   it 'can initialize' do
     expect(@item1).to be_an_instance_of(Item)
     expect(@item2).to be_an_instance_of(Item)
+    expect(@item3).to be_an_instance_of(Item)
+    expect(@item4).to be_an_instance_of(Item)
+    expect(@item5).to be_an_instance_of(Item)
   end
 
   it 'has a name' do
     expect(@item1.name).to eq('Chalkware Piggy Bank')
     expect(@item2.name).to eq('Bamboo Picture Frame')
+    expect(@item3.name).to eq('Homemade Chocolate Chip Cookies')
+    expect(@item4.name).to eq('2 Days Dogsitting')
+    expect(@item5.name).to eq('Forever Stamps')
   end
 
 end
